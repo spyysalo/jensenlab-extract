d7ccade2821f15ff2b3b906f7c146c591d060305
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2019-03-06 15:47:00 +0200

    Robustness

--- run_popup_examples.sh
+++ run_popup_examples.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-INDIR=examples
-OUTDIR=popup-examples-output
+set -euo pipefail
+
+# https://stackoverflow.com/a/246128
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+INDIR="$BASEDIR/examples"
+OUTDIR="$BASEDIR/popup-examples-output"
 
 mkdir -p "$OUTDIR"
 
-python3 scripts/getpopup.py -d "$OUTDIR" "$INDIR"/*.txt
+python3 "$BASEDIR/scripts/getpopup.py" -d "$OUTDIR" "$INDIR"/*.txt
 
 echo "Done, results in $OUTDIR" >&2
