4ece46854e5e08b30312c73db3163e8c1f24f5bb
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2019-03-16 16:22:20 +0200

    Commit at given intervals

--- scripts/makedb.py
+++ scripts/makedb.py
@@ -5,26 +5,38 @@ import os
 
 from logging import error
 
+try:
+    import sqlitedict
+except ImportError:
+    error('failed to import sqlitedict; try `pip3 install sqlitedict`')
+    raise
+
+
+DEFAULT_INTERVAL = 10**6
+
 
 def argparser():
     from argparse import ArgumentParser
     ap = ArgumentParser()
+    ap.add_argument('-i', '--commit-interval', metavar='INT', type=int,
+                    default=DEFAULT_INTERVAL,
+                    help='number of items to input between commits')
     ap.add_argument('dict', help='input dictionary in key-value TSV format')
     ap.add_argument('dbname', help='output database name')
     return ap
 
 
-def process(in_, dbname, lines, options):
-    try:
-        import sqlitedict
-    except ImportError:
-        error('failed to import sqlitedict; try `pip3 install sqlitedict`')
-        raise
-
-    seen_keys = set()
-    print('Reading from {} ...'.format(in_.name), file=sys.stderr)
+def process_interval(in_, dbname, idx, end, limit, options):
+    seen_keys = process_interval.seen_keys
+    end = min(end, limit)
     with sqlitedict.SqliteDict(dbname, autocommit=False) as db:
-        for ln, line in enumerate(in_, start=1):
+        while idx < end:
+            ln = idx + 1
+            try:
+                line = next(in_)
+            except StopIteration:
+                error('unexpected EOF in {} at line {}'.format(in_.name, ln))
+                break
             try:
                 fields = line.rstrip('\n').split('\t')
                 key, value = fields
@@ -36,14 +48,30 @@ def process(in_, dbname, lines, options):
                 error('on line {} in {}: {}'.format(ln, in_.name, line))
                 raise
             if ln % 1024 == 0:
-                print('Read {}/{} ({:.1%}) lines'.format(ln, lines, ln/lines),
+                print('Read {}/{} ({:.1%}) lines'.format(ln, limit, ln/limit),
                       end='\r', file=sys.stderr, flush=True)
-        print('Read {}/{} ({:.0%}) lines, committing...'.format(
-            ln, lines, ln/lines), end='', file=sys.stderr, flush=True)
+            idx += 1
+        print('Read {}/{} ({:.1%}) lines, committing...'.format(
+            ln, limit, ln/limit), end='', file=sys.stderr, flush=True)
         db.commit()
-    print('done.', file=sys.stderr)
-    print('Print read {}, stored {} unique ({:.1%}).'.format(
-        ln, len(seen_keys), len(seen_keys)/ln, file=sys.stderr))
+        print('done.', file=sys.stderr)
+    return idx
+process_interval.seen_keys = set()
+
+
+def process(in_, dbname, total_lines, options):
+    print('Reading from {} ...'.format(in_.name), file=sys.stderr)
+    idx, interval = 0, options.commit_interval
+    while idx < total_lines:
+        nxt = process_interval(in_, dbname, idx, idx+interval, total_lines,
+                               options)
+        if nxt == idx:
+            break    # failed to progress
+        idx = nxt
+    unique = len(process_interval.seen_keys)
+    ratio = 0 if idx == 0 else unique/idx
+    print('Finished: read {}, stored {} unique ({:.1%}).'.format(
+        idx, unique, ratio, file=sys.stderr))
 
 
 def count_lines(fn):
